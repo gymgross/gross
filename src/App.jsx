5a1ad5eb4972205572ce1467c826b264b1f9eec7
mapz94 <miguel.apg94@gmail.com>
2023-12-29 19:10:46 -0300

    adds env value & base path

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ console.log(planes)
 
 function App() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  function basePrefix(e) {
+    return `${import.meta.env.BASE_URL}${e}`;
+  }
   const handleScroll = () => {
       const position = window.scrollY;
       setScrollPosition(position);
@@ -19,7 +22,7 @@ function App() {
   return (
     <>
     <div id="sticky" className={`fixed z-50 bg-black transition-transform transform ${scrollPosition < 16 ? "-translate-y-full": "translate-y-0"}`}>
-        <img className="w-1/3 contrast-200 m-auto" src="/logo.webp" alt="Logo" />
+        <img className="w-1/3 contrast-200 m-auto" src={basePrefix("logo.webp")} alt="Logo" />
     </div>
     <div id="header" className="w-full hidden">
         <img className="contrast-200 sm:w-1/2 m-auto" src="/logo.webp" alt="Logo" />
@@ -41,15 +44,15 @@ function App() {
                 </div>
             </div>
             <div className="relative contrast-200 w-full scale-100"  id="welcomeVideoParent">
-                <video poster="/op.webp" loop disablePictureInPicture autoPlay muted
-                    src="/op.webm"></video>
+                <video poster={basePrefix("op.webp")} loop disablePictureInPicture autoPlay muted
+                    src={basePrefix("op.webm")}></video>
             </div>
         </div>
         <div>
             <h1>Planes</h1>
         </div>
         <div className="snap-x overflow-scroll w-full flex gap-4 mx-4 no-scrollbar scrolling-touch no-drag px-4">
-            {planes.map((plan,i) => <img className="snap-start shrink-0 " src={plan.image} key={i} />)}
+            {planes.map((plan,i) => <img className="snap-start shrink-0 " src={basePrefix(plan.image)} key={i} />)}
         </div>
     </div>
     </>
