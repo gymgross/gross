5d6ebc55917e9cb81791f8db0d595add532f0a02
mapz94 <miguel.apg94@gmail.com>
2024-01-05 17:42:13 -0300

    overahaul

---
+++ public/index.js
@@ -0,0 +1,4 @@
+const map = document.getElementById("map")
+console.log(map)
+console.log("found")
+map.innerHTML += '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.525256468741!2d-70.6187487!3d-33.461673100000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf8ad7efb5a7%3A0x991c5dc2e087a184!2sAv.%20Grecia%201327%2C%207780049%20%C3%91u%C3%B1oa%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1sen!2scl!4v1704485396460!5m2!1sen!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react'
+import { Document, Page } from 'react-pdf';
 import './App.css'
 import planes from './assets/planes.json'
 
@@ -8,26 +9,75 @@ function App() {
   function basePrefix(e) {
     return `${import.meta.env.BASE_URL}${e}`;
   }
-  /*const getAssetSrc = (name) => {
+  const getAssetSrc = (name) => {
     const path = `/src/assets/${name}`;
     const modules = import.meta.glob("/src/assets/**", { eager: true });
     const mod = modules[path];
     return mod.default;
-};*/
-  const handleScroll = () => {
+  };
+  const getYScrollPos = () => {
       const position = window.scrollY;
       setScrollPosition(position);
   };
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener('scroll', getYScrollPos, { passive: true });
     return () => {
-        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('scroll', getYScrollPos);
     };
   }, []);
-  return (
+  const PDFViewer = () => {
+    const pdfURL = basePrefix("terminos&condiciones.pdf");
+    return (
+        <div>
+            <Document file={pdfURL}>
+                <Page></Page>
+            </Document>
+        </div>
+    );
+  }
+
+  const whatsapplink = "https://wa.me/56986671104"
+
+  function sendWhatsapp(e, message){
+    const text = message ? `?text=${encodeURIComponent(message)}` : ""
+    window.open(`${whatsapplink}${text}`, "_blank")
+  }
+
+  // Function to handle smooth scroll to anchors
+  const scrollToAnchor = (e, anchorId) => {
+    e.preventDefault()
+    const anchorElement = document.getElementById(anchorId);
+    if (anchorElement) {
+      const navbarHeight = 282 // document.getElementById("navbar").offsetHeight // navbarRef.current.offsetHeight;
+      const offsetTop = anchorElement.offsetTop - navbarHeight;
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+  const anchors = [
+    "inicio",
+    "planes",
+    "servicios",
+    "contacto"
+  ]
+return (
     <>
-    <div id="sticky" className={`w-screen fixed z-50 bg-black transition-transform transform ${scrollPosition < 16 ? "-translate-y-full": "translate-y-0"}`}>
-        <img className="w-1/3 contrast-200 m-auto" src={basePrefix("logo.webp")} alt="Logo" />
+    <div id="navbar" className={`w-screen fixed z-50 bg-black navbar flex-col p-0`}>
+        <img className={`p-0 w-64 contrast-200 m-auto transition-transform transform ${scrollPosition < 16 ? "-translate-y-full h-0": "translate-y-0 h-full"}`} src={basePrefix("logo.webp")} alt="Logo" />
+        <div className="navbar-start w-full">
+            <div className="m-auto">
+                <ul className="menu menu-horizontal px-1">
+                    {anchors.map((anchor,i) => {
+                        return (
+                            <li className="text-xl uppercase text-white border-x border-1px" key={i}><a href={`#${anchor}`} onClick={(e) => scrollToAnchor(e, anchor)}>{anchor}</a></li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </div>
     </div>
     <div id="header" className="w-full hidden">
         <img className="contrast-200 sm:w-1/2 m-auto" src="/logo.webp" alt="Logo" />
@@ -41,7 +91,7 @@ function App() {
             </div>
         </div>
     </div>
-    <div className="grid grid-rows-auto w-full">
+    <div id='inicio' className="grid grid-rows-auto w-full">
         <div id="hero" className="relative overflow-hidden h-screen flex -mb-8">
             <div className=" hidden absolute z-30 grid grid-rows-5 h-full w-full">
                 <div className="text-5xl text-center text-white justify-self-center self-center row-span-3">
@@ -49,31 +99,83 @@ function App() {
                 </div>
             </div>
             <div className="relative contrast-200 w-full scale-100 self-center"  id="welcomeVideoParent">
-                <video poster={basePrefix("op.webp")} loop disablePictureInPicture autoPlay muted
+                <video className='max-md:hidden' poster={basePrefix("op.webp")} loop disablePictureInPicture autoPlay muted
                     src={basePrefix("op.webm")}></video>
+                <video className='md:hidden' poster={basePrefix("op.webp")} loop disablePictureInPicture autoPlay muted
+                    src={basePrefix("op_mobile.webm")}></video>
+
             </div>
         </div>
-        <div className="text-white h-screen snap-x overflow-scroll w-full flex mx-4 no-scrollbar scrolling-touch no-drag px-4 scale-90">
-            {planes.map((plan,i) => {
-                return  <>
-                        <div key={i} className="snap-start shrink-0 span  overflow-hidden relative  w-screen">
-                            <img className='absolute object-cover h-full' src={plan.image}></img>
-                            <div className={`absolute top-0 ${i % 2 != 0 ? "left-0" : "right-0"} h-full w-2/5 bg-black opacity-80 p-8 text-center pt-16`}>
-                                <h2 className='text-xl md:text-4xl font-bold uppercase from-yellow-400 to-red-500 text'>
-                                    {plan.nombre}
-                                </h2>
-                                {plan.notas.map((note,j) => {
-                                    return <>
-                                        <h3 className='md:text-2xl md:mt-4' key={j}>{note}</h3> 
-                                    </>
-                                })}
-                                <h2 className='text-2xl md:text-4xl md:mt-8'>
-                                    {plan.precio} Mensual
-                                </h2>
+
+    </div>
+    <div id='planes' className="w-full p-4">
+        <a href='#planes' onClick={(e) => scrollToAnchor(e, "planes")}>
+            <h1 className='text-4xl text-white m-[1rem] font-bold'># Nuestros Planes</h1>
+        </a>
+        <div className="w-full overflow-x-scroll">
+            <div className='carousel w-fit mx-auto p-4'>
+                {planes.map((plan,i) => {
+                    return ( 
+                    <div key={i} className="carousel-item relative select-none">
+                        <div className="card card-bordered w-96 mx-2">
+                            {() => {
+                                if(plan.image_url){
+                                    return (
+                                        <figure>
+                                            <img src={plan.image_url} alt="Plan Background" />
+                                        </figure>
+                                    )
+                                }
+                                return ""
+                            }}
+                            <div className="card-body text-white text-center border border-white border-1px rounded ">
+                                <div className="text-2xl font-bold ">
+                                    Plan: {plan.name}
+                                </div>
+                                <div className='text-xl'>Matricula: {plan.registration_fee ? "$20.000" : "GRATIS"}</div> 
+                                <div className="text-xl">Horarios: {plan.schedule}</div>
+                                <div className='text-xl'>{() => {
+                                    if(plan.features.length){
+                                        return "Incluye:"
+                                    }
+                                    return ""
+                                }} {plan.features.map((feature,i) => {
+                                    return ( <div key={i} className=" badge bg-orange-600 text-white">{feature}</div>)
+                                })} </div>
+                                <div className="text-xl mt-4">{plan.cost}</div>
+                                <div className="card-actions mt-4 ">
+                                    <div onClick={(e) => sendWhatsapp(e, `Hola, me interesa el plan ${plan.name} y quisiera unirme a Gross Gym. ¿Podría obtener mas información?`)} className="m-auto btn text-white text-xl capitalized bg-orange-600">Me Interesa</div>
+                                </div>
                             </div>
-                        </div>
-                    </> 
-            })}
+                        </div>            
+                    </div>)
+                })}
+            </div>
+        </div>
+    </div>
+    <div id="contacto" className="w-full p-4 text-white">
+        <a href="#contacto" onClick={(e) => scrollToAnchor(e, "contacto")}>
+            <h1 className='text-4xl font-bold'>
+                # Contacto
+            </h1>
+        </a>
+        <div className="flex-col w-full">
+            <div className="text-4xl text-white font-bold text-center">
+                Estamos aquí para ayudarte a alcanzar tus objetivos de fitness. 
+            </div>
+            <div className="my-2 text-4xl text-white font-bold text-center">
+                Contactanos a:
+            </div>
+            <div className="w-full flex-col text-center">
+                <div onClick={(e) => sendWhatsapp(e, "Hola, me interesa unirme a Gross Gym.")} className="btn bg-green-600 text-white text-2xl my-4">
+                    <img className='w-8 invert' src={getAssetSrc("whatsapplogo.png")} alt="" />Whatsapp
+                </div>
+                <div className="text-white text-4xl font-bold my-4">
+                    <a href="" onClick={() => window.open("tel:+56:986671104", "_blank")}><h1>{`${"+56 9 86671104"}`}</h1></a>
+                </div>
+            </div>
+        </div>
+        <div id='map' className='w-full'>
         </div>
     </div>
     </>
